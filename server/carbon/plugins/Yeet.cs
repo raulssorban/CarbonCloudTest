3bd4b0586acad4b62b288ff429ebe0604292e986
raul <raulssorban@gmail.com>
2023-07-17 16:50:42 +0200

    no message

--- server/carbon/plugins/Yeet.cs
+++ server/carbon/plugins/Yeet.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 namespace Carbon.Plugins;
 
-[Info("Yeet", "Not Raul don't sue me", "1.0.0")]
+[Info ( "Yeet", "Not Raul don't sue me", "1.0.0" )]
 public class Yeet : CarbonPlugin
 {
-	private void OnServerInitialized()
-	{
-		ConsoleSystem.Run("quit");
-	}
+    private void OnServerInitialized ()
+    {
+        ConsoleSystem.Run(ConsoleSystem.Option.Server, "quit" );
+    }
 }
